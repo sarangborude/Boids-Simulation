0f748b40afe2c2c5cc3087acf41340d4565f00e6
sarangborude <sarang.borude@gmail.com>
2025-10-03 15:59:35 -0700

    Initial Commit

---
+++ Boids Simulation/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Boids Simulation
+//
+//  Created by Sarang Borude on 10/3/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Boids Simulation/Boids_SimulationApp.swift
@@ -0,0 +1,33 @@
+//
+//  Boids_SimulationApp.swift
+//  Boids Simulation
+//
+//  Created by Sarang Borude on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Boids_SimulationApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
